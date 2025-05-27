5cd7bdcc08787d103505d25b61c0a22d43525f47
Nazar Zozulya <likeemangames@gmail.com>
2025-05-27 19:38:34 +0300

    починил post card

--- src/modules/userpost/hooks/usePost.tsx
+++ src/modules/userpost/hooks/usePost.tsx
@@ -1,22 +1,23 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Result } from '../../auth/types';
 import { IPostCart, IPostCartForm } from '../types';
+import { SERVER_HOST } from '../../../shared/constants'
 
 export function usePost() {
-    const [posts, setPosts] = useState<IPostCart[]>([]);
+	const [posts, setPosts] = useState<IPostCart[]>([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState<string | null>(null);
-    const [ refresh, setRefresh ] = useState(false)
-    
-    
+	const [refresh, setRefresh] = useState(false)
+
+
 	async function createPost(data: IPostCartForm): Promise<Result<IPostCart> | undefined> {
 		try {
-			const response = await fetch("http://192.168.3.4:8000/api/posts/create", {
+			const response = await fetch(`${SERVER_HOST}api/posts/create`, {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify(data),
 			});
-            // console.log(data)
+			// console.log(data)
 
 			const result: Result<IPostCart> = await response.json();
 
@@ -26,56 +27,56 @@ export function usePost() {
 			}
 
 			if (result.status === "success") {
-                // Вариант 1: добавляем вручную
+				// Вариант 1: добавляем вручную
 				// setPosts(prev => [...prev, result.data]);
-                
+
 				// // Вариант 2: или перезапрашиваем все посты
 				// await getAllPosts();
-                // console
-                
-                
-                const all = await getAllPosts();
-                
-                // console.log("New posts:", posts);
+				// console
+
+
+				const all = await getAllPosts();
+
+				// console.log("New posts:", posts);
 				return result;
 			}
 		} catch (error) {
-            console.error("Create Post Error:", error);
+			console.error("Create Post Error:", error);
 			return { status: "error", message: "An unexpected error occurred" };
 		}
 	}
-    
-    // const getAllPosts = useCallback(
-    async function getAllPosts(): Promise<Result<IPostCart[]> | undefined> {
-        
-        try {
-            setIsLoading(true);
-            const response = await fetch("http://192.168.3.4:8000/api/posts/all");
-            
-            const result: Result<IPostCart[]> = await response.json();
-            
-            if (result.status === "error") {
-                setError(result.message);
-                console.log(result.message);
-                return result;
-            } else {
-                setRefresh(false)
-                setPosts(result.data);
-                console.log(13123123)
-                return result;
-            }
-            
-        } catch (error) {
-            console.error("Get All Posts Error:", error);
-            return { status: "error", message: "An unexpected error occurred" };
-        } finally {
-            setIsLoading(false);
-        }
-    }
-    // [])
+
+	// const getAllPosts = useCallback(
+	async function getAllPosts(): Promise<Result<IPostCart[]> | undefined> {
+
+		try {
+			setIsLoading(true);
+			const response = await fetch(`${SERVER_HOST}api/posts/all`);
+
+			const result: Result<IPostCart[]> = await response.json();
+
+			if (result.status === "error") {
+				setError(result.message);
+				console.log(result.message);
+				return result;
+			} else {
+				setRefresh(false)
+				setPosts(result.data);
+				console.log(13123123)
+				return result;
+			}
+
+		} catch (error) {
+			console.error("Get All Posts Error:", error);
+			return { status: "error", message: "An unexpected error occurred" };
+		} finally {
+			setIsLoading(false);
+		}
+	}
+	// [])
 	async function getPost(id: number): Promise<Result<IPostCart> | undefined> {
 		try {
-			const response = await fetch(`http://192.168.3.4:8000/api/posts/${id}`);
+			const response = await fetch(`${SERVER_HOST}api/posts/${id}`);
 			const result: Result<IPostCart> = await response.json();
 
 			if (result.status === "error") {
@@ -92,7 +93,7 @@ export function usePost() {
 
 	async function deletePost(id: number): Promise<Result<string> | undefined> {
 		try {
-			const response = await fetch("http://192.168.3.4:8000/api/posts/delete", {
+			const response = await fetch(`${SERVER_HOST}api/posts/delete`, {
 				method: "DELETE",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify(id),
@@ -116,7 +117,7 @@ export function usePost() {
 
 	async function changePost(data: IPostCart): Promise<Result<IPostCart> | undefined> {
 		try {
-			const response = await fetch("http://192.168.3.4:8000/api/posts/change", {
+			const response = await fetch(`${SERVER_HOST}api/posts/change`, {
 				method: "PUT",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify(data),
@@ -144,9 +145,9 @@ export function usePost() {
 		getAllPosts();
 	}, []);
 
-    // useEffect(() => {
-    //     console.log(posts)
-    // }, [posts])
+	// useEffect(() => {
+	//     console.log(posts)
+	// }, [posts])
 
 	return {
 		posts,
@@ -157,7 +158,7 @@ export function usePost() {
 		getPost,
 		deletePost,
 		changePost,
-        refresh,
-        setRefresh,
+		refresh,
+		setRefresh,
 	};
 }
---
+++ src/shared/constants/base.ts
@@ -0,0 +1,1 @@
+export const SERVER_HOST = 'http://192.168.0.100:8000/'
